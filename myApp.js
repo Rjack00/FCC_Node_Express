81faa17a5de9b95dbddbc9eb7024b44bf63282f8
Rjack00 <Rjackson77@comcast.net>
2025-09-04 18:58:43 -0600

    myApp.js updated

--- myApp.js
+++ myApp.js
@@ -1,9 +1,15 @@
+//These first 2 lines create an Express app object named app
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+console.log("Hello World"); //Testing app
 
-
+//This object has several methods. Here are some examples:
+app.get('/', (req,res) => {
+    //res.send("Hello Express");
+    const absolutePath = __dirname + '/views/index.html';//__dirname is the Node global variable
+    res.sendFile(absolutePath);
+});
 
 
 
