9c3be0ca8f2b4a9685ee3df50dd9033ce3d0d689
Rjack00 <Rjackson77@comcast.net>
2025-09-08 18:37:38 -0600

    myApp.js updated

--- myApp.js
+++ myApp.js
@@ -2,15 +2,49 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World"); //Testing app
+let path = require('path');//Node.js module to handle file paths safely across operating systems
+require('dotenv').config();//Loads variables from .env file. dotenv was installed and located in package.json
 
-//This object has several methods. Here are some examples:
-app.get('/', (req,res) => {
-    //res.send("Hello Express");
-    const absolutePath = __dirname + '/views/index.html';//__dirname is the Node global variable
-    res.sendFile(absolutePath);
+// Note: Express evaluates functions in the order they appear in the code. 
+
+// The following is a "Middleware" function (it includes "next" function)
+// This example executes for every request below it.
+app.use((req, res, next) => {
+    console.log(`${req.method} /${req.path} - ${req.ip}`);
+
+    next();
 });
 
+console.log("Hello World"); //Testing app (only shows in vscode terminal not browser console).
+
+//\\\\\\\\\\\\\\\
+//  This object has several methods. Here are some examples:
+
+//\\\\\\\\\\\\\\\
+//  app.get retrieves data. In this case index.html to be used for this express app.
+
+// app.get('/', (req,res) => {
+//     //res.send("Hello Express"); //prints "Hello Express" on the browser window.
+//     const absolutePath = __dirname + '/views/index.html';//__dirname is the Node global variable
+
+//     res.sendFile(absolutePath);//response results in file.
+// });
+
+//\\\\\\\\\\\\\\\\
+//  app.use results in using the style.css file located in /public for this express app
+app.use('/public', express.static(path.join(__dirname + '/public')));//path.join handles path separators (/ or \) correctly for your operating system.
+
+//\\\\\\\\\\\\\\\\\\
+//  resulted in web page displaying myObj as json when url changed to http://localhost:3000/json
+app.get('/json', (req, res) => {
+    let myObj = {"message": "Hello json"};
+    //process.env.MESSAGE_STYLE variable accessible through require('dotenv').config()
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+       myObj.message = myObj.message.toUpperCase();
+    }
+
+    res.json(myObj);
+});
 
 
 
